ec83962108b224b5359455fc23753676d46d3db3
Micah <82362825+TheXenonDragon@users.noreply.github.com>
2023-12-04 23:07:09 -0600

    New Music Added
    Last Update before implementing Music Editor Package.  December Update.

--- Assets/Scripts/Controllers/BeatController.cs
+++ Assets/Scripts/Controllers/BeatController.cs
@@ -5,28 +5,84 @@ using UnityEngine;
 
 public class BeatController : MonoBehaviour
 {
+    //  Old
     public event EventHandler BeatExecuted;
-
-    public float primarySpacing = 1.4f;
-    public float secondarySpacing = 1.4f;
-
-    private float[] waitTimes;
-
-    AudioSource audioSource;
-
-    private static int index = 0;
-
     private bool isPaused = false;
+
+    //  New
+    //  Song Beats Per Minute
+    public float songBpm;
+
+    //  How long each beat lasts in seconds
+    public float secPerBeat;
+
+    //  The Current song position, in seconds
+    public float songPosition;
+
+    //  The Current song position, in beats
+    public float songPositionInBeats;
+
+    //  How many seconds have passed since the song started
+    public float dspSongTime;
+    
+    //The offset to the first beat of the song in seconds
+    public float firstBeatOffset;
+
+    //  Current Song
+    private AudioSource audioSource;
+
+    //  List of available songs.
+    public AudioClip[] songs;
+    
+    public int songIndex;
+
+    private int beatIndex = 0;
+
+    private float[] beats;
+
 
 
     // Start is called before the first frame update
     void Start()
     {
-        waitTimes = new float[] {primarySpacing, primarySpacing, secondarySpacing, secondarySpacing};
+        beats = new float[2000];
+        for(int i = 0; i < beats.Length; i++){
+            beats[i] = (float)i * 2;
+        }
+
         audioSource = GetComponent<AudioSource>();
-        StartCoroutine(BeatTimer());
+
+        //  Set song for audio source.
+        audioSource.clip = songs[songIndex];
+
+        //Calculate the number of seconds in each beat
+        secPerBeat = 60f / songBpm;
+
+        //Record the time when the music starts
+        dspSongTime = (float)AudioSettings.dspTime;
+
+        //Start the music
+        audioSource.Play();
     }
 
+    void Update()
+    {
+        //determine how many seconds since the song started
+        songPosition = (float)(AudioSettings.dspTime - dspSongTime - firstBeatOffset);
+
+        //determine how many beats since the song started
+        songPositionInBeats = songPosition / secPerBeat;
+
+        if (beatIndex < beats.Length && beats[beatIndex] < songPositionInBeats + 0)
+        {
+            print($"Note: {beatIndex}");
+            BeatExecuted.Invoke(this, null);
+
+            beatIndex++;
+        }
+    }
+
+    /*
     IEnumerator BeatTimer(){
         while(true){
             if(!isPaused){
@@ -48,6 +104,7 @@ public class BeatController : MonoBehaviour
     private void PlayAudio(){
         audioSource.Play();
     }
+    */
 
     public void Pause(){
         isPaused = true;
--- Assets/Scripts/UI/PlayerHUD.cs
+++ Assets/Scripts/UI/PlayerHUD.cs
@@ -6,30 +6,146 @@ using TMPro;
 
 public class PlayerHUD : MonoBehaviour
 {
+    //  Scripts
     public HealthManager healthManager;
-    public WeaponManager weaponManager;
-    public XPManager xPManager;
 
-    public Slider healthSlider;
-    public Slider weaponSlider;
+    //  Notes
+    public GameObject notePrefab;
+    private RectTransform[] notes;
+    private Image[] noteImages;
 
-    public TextMeshProUGUI weaponDamageLevel;
-    public TextMeshProUGUI xpText;
+    //  Hud Elements
+    public RectTransform hudContainer;
+    public RectTransform beatBar;
+    public RectTransform heartContainer;
+    public RectTransform[] hearts;
+    public RectTransform energyContainer;
+    public RectTransform[] energies;
 
+    //  Cached Values
+    int screenPixelWidth = 0;
+    int screenPixelHeight = 0;
+    int healthCount = 0;
+    private float noteWidth = 3f;
+    
+    //  Color
+    private Color energyBlue = new Color(9f / 255f, 246f / 255f, 219f / 255f);
+    private Color heartRed = new Color(246f / 255f, 9f / 255f, 36f / 255f);
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        healthSlider.maxValue = healthManager.HealthCount();
-        weaponSlider.maxValue = weaponManager.LevelUpAtLevel();
+        healthCount = healthManager.HealthCount();
+        UpdateHudSizeAndPosition();
+        UpdateHealth();
+        CreateNotes();
     }
 
     // Update is called once per frame
     void Update()
     {
-        healthSlider.value = healthManager.HealthCount();
-        weaponSlider.value = weaponManager.CurrentWeaponLevel();
-        weaponDamageLevel.text = $"{weaponManager.CurrentWeaponDamage()}";
-        xpText.text = $"{xPManager.GetXP()}";
+        UpdateHudSizeAndPosition();
+        UpdateHealth();
+        UpdateNotes();   
+    }
+
+    private void UpdateHudSizeAndPosition(){
+        if(screenPixelWidth != Screen.width || screenPixelHeight != Screen.height){
+            int xOffset = 10;
+            int yOffset = 5;
+            float heartSize;
+            float energySize;
+
+            //  Hud Container
+            screenPixelWidth = Screen.width;
+            screenPixelHeight = Screen.height;
+            hudContainer.sizeDelta = new Vector2(screenPixelWidth + xOffset, screenPixelHeight / 8);
+            hudContainer.anchoredPosition = new Vector2(0, - (screenPixelHeight / 2) + (hudContainer.sizeDelta.y / 2) - yOffset);
+
+            //  Beat Bar
+            beatBar.sizeDelta = new Vector2(hudContainer.sizeDelta.x / 2, hudContainer.sizeDelta.y / 2);
+
+            //  Heart Container
+            heartContainer.sizeDelta = new Vector2(beatBar.sizeDelta.x / 2, hudContainer.sizeDelta.y);
+            heartContainer.anchoredPosition = new Vector2(-(hudContainer.sizeDelta.x - beatBar.sizeDelta.x) * 0.75f, 0);
+
+            //  Hearts
+            if((heartContainer.sizeDelta.y - yOffset) < ((heartContainer.sizeDelta.x - heartContainer.sizeDelta.y)) / 5){
+                heartSize = hudContainer.sizeDelta.y / 2;
+            }
+            else{
+                heartSize = ((heartContainer.sizeDelta.x - heartContainer.sizeDelta.y)) / 5;
+            }
+
+            for(int i = 0; i < hearts.Length; i++){
+                hearts[i].sizeDelta = new Vector2(heartSize, heartSize);
+                hearts[i].anchoredPosition = new Vector2((((heartContainer.sizeDelta.x - heartSize)) / 5) * (i - 2), 0);
+            }
+
+            //  Energy Container
+            energyContainer.sizeDelta = new Vector2(beatBar.sizeDelta.x / 2, hudContainer.sizeDelta.y);
+            energyContainer.anchoredPosition = new Vector2((hudContainer.sizeDelta.x - beatBar.sizeDelta.x) * 0.75f, 0);
+
+            //  Energy
+            if((energyContainer.sizeDelta.y - yOffset) < ((energyContainer.sizeDelta.x - energyContainer.sizeDelta.y)) / 5){
+                energySize = hudContainer.sizeDelta.y / 2;
+            }
+            else{
+                energySize = ((energyContainer.sizeDelta.x - energyContainer.sizeDelta.y)) / 5;
+            }
+
+            for(int i = 0; i < energies.Length; i++){
+                energies[i].sizeDelta = new Vector2(energySize, energySize);
+                energies[i].anchoredPosition = new Vector2((((energyContainer.sizeDelta.x - energySize)) / 5) * (-i + 2), 0);
+            }
+        }
+    }
+
+    private void UpdateHealth(){
+        if(healthCount != healthManager.HealthCount()){
+            healthCount = healthManager.HealthCount();
+
+            if(healthCount < 0){
+                healthCount = 0;
+            }
+
+            for(int i = 0; i < healthCount; i++){
+                hearts[i].gameObject.SetActive(true);
+            }
+
+            for(int i = healthCount; i < hearts.Length; i++){
+                hearts[i].gameObject.SetActive(false);
+            }
+        }
+    }
+
+    private void CreateNotes(){
+        notes = new RectTransform[30];
+        noteImages = new Image[notes.Length];
+        for(int i = 0; i < notes.Length; i++){
+            notes[i] = Instantiate<GameObject>(notePrefab, Vector3.zero, notePrefab.transform.rotation, beatBar.gameObject.transform).GetComponent<RectTransform>();
+            notes[i].sizeDelta = new Vector2(noteWidth, beatBar.sizeDelta.y * 0.8f);
+            notes[i].anchoredPosition = new Vector2(i * noteWidth * 5, 0);
+            noteImages[i] = notes[i].gameObject.GetComponent<Image>();
+            noteImages[i].color = energyBlue;
+        }
+    }
+
+    private void UpdateNotes(){
+        float speed = -100f;
+        for(int i = 0; i < notes.Length; i++){
+            notes[i].sizeDelta = new Vector2(noteWidth, beatBar.sizeDelta.y * 0.8f);
+            notes[i].anchoredPosition = new Vector2(notes[i].anchoredPosition.x + (speed * Time.deltaTime), 0);
+
+            if(notes[i].anchoredPosition.x < -beatBar.sizeDelta.x / 2){
+                notes[i].anchoredPosition = new Vector2((notes[i].sizeDelta.x / 2) + (beatBar.sizeDelta.x / 2), 0);
+                noteImages[i].color = energyBlue;
+            }
+
+            if(notes[i].anchoredPosition.x < 0){
+                noteImages[i].color = heartRed;
+            }
+        }
     }
 }
